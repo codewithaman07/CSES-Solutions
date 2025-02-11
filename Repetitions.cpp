638e27ab2eb29116828ddf2bf34dd83ba7681ec5
codewithaman07 <amansingh2009000@gmail.com>
2025-02-11 22:52:31 +0530

    .

---
+++ Repetitions.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define Aman() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define endl '\n'
+#define int long long
+#define pb push_back
+#define f(x,n) for(int i = x ; i<n ; i++)
+#define all(x) x.begin(),x.end()
+const int M = 1e9+7;
+const int N = 1e6 + 1;
+
+
+void solve() {
+    string s; cin>>s;
+    int cnt = 1, ans = 1;
+    for(int i = 1; i<s.size(); i++){
+        if(s[i] == s[i-1]) cnt++;
+        else cnt = 1;
+        ans = max(ans,cnt);
+    }
+    cout<<ans<<endl;
+}
+
+signed main() {
+    Aman();
+
+    int tc = 1;
+    // cin >> tc;
+    while (tc--) {
+        solve();
+    }
+    return 0;
+}
