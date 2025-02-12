da40b1c0e5f5482fc8fe6ddb2ab338d5e61da5e7
codewithaman07 <amansingh2009000@gmail.com>
2025-02-12 23:07:23 +0530

    .

---
+++ Stick_Lengths.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+#define Aman() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define endl '\n'
+#define int long long
+#define pb push_back
+#define all(x) x.begin(), x.end()
+const int M = 1e9+7;
+const int N = 1e6 + 1;
+
+void solve() {
+    int n; cin>>n;
+    vector<int>arr(n);
+    for(int i = 0; i<n; i++) cin>>arr[i];
+    sort(all(arr));
+    vector<int>presum(n+1,0);
+    for(int i = 0; i<n; i++){
+        presum[i+1] = presum[i] + arr[i];
+    }
+    int ans = 1e18;
+    for(int i = 0; i<n; i++){
+        ans = min(ans, abs(arr[i]*(i) - presum[i]) + abs(presum[n] - presum[i] - arr[i]*(n-i)));
+    }
+    cout<<ans<<endl;
+}
+
+signed main() {
+    cout << fixed << setprecision(20);
+    Aman();
+
+    int tc = 1;
+    while (tc--) {
+        solve();
+    }
+    return 0;
+}
